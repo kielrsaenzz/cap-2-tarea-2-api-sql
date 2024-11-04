1c4d857757ab9e831c8763444652accec765bc17
Kiel R. Sáenz <kiel.saenz@yahoo.com>
2024-11-03 22:19:57 -0500

    =Solución Tarea 2

--- app.py
+++ app.py
@@ -1,74 +1,139 @@
-import sqlite3
-from flask import Flask, render_template, abort, request, url_for
-# from db import get_db_connection
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    request,
+    url_for,
+    send_from_directory,
+)
+from db import get_db_connection
+import os
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def index():
-        return render_template('index.html')
+    return redirect(url_for("home"))
 
-@app.route('/home', methods=['GET'])
+
+@app.route("/home", methods=["GET"])
 def home():
-    return render_template('home.html')
+    return render_template("home.html", active_page="home")
 
-@app.route('/post', methods=['GET'])
+
+@app.route("/posts", methods=["GET"])
 def get_all_post():
     conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchone()
+    posts = conn.execute("SELECT * FROM post").fetchall()
     conn.close()
-    return render_template('post/posts.html', posts=posts)
+    # Se captura el mensaje de éxito si es que existe
+    success_msg = request.args.get("success_msg")
+    return render_template(
+        "post/posts.html", posts=posts, active_page="posts", success_msg=success_msg
+    )
 
-@app.route('/post/<int:post_id>', methods=['GET'])
+
+@app.route("/post/<int:post_id>", methods=["GET"])
 def get_one_post(post_id):
     conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    post = conn.execute("SELECT * FROM post WHERE id = ?", (post_id,)).fetchone()
     conn.close()
-    return render_template('post/post.html', post=post)
+    # Se valida que exista el Post
+    if post is None:
+        return redirect(url_for("get_all_post"))
+    return render_template("post/post.html", post=post)
 
-@app.route('/post/create', methods=['POST'])
+
+@app.route("/post/create", methods=["GET", "POST"])
 def create_one_post():
-    if request.method == 'POST':
-        title = request.form['title']
-        contend = request.form['contend']
+    if request.method == "POST":
+        title = request.form["title"].strip()
+        content = request.form["content"].strip()
+        # Se valida el título y el contenido
+        if not title or not content:
+            error_msg = "Por favor, ingrese un " + (
+                "título y un contenido."
+                if not title and not content
+                else "título." if not title else "contenido."
+            )
+            return render_template(
+                "/post/create.html",
+                title=title,
+                content=content,
+                error_msg=error_msg,
+            )
+
         conn = get_db_connection()
-        conn.execute('INSERT INTO posts (title, content) VALUES (?, !)', (title, contend))
+        conn.execute(
+            "INSERT INTO post (title, content) VALUES (?, ?)", (title, content)
+        )
         conn.commit()
         conn.close()
-        return redirect(url_for('/post'))
+        return redirect(
+            url_for("get_all_post", success_msg="Se ha creado el Post exitosamente.")
+        )
 
-    return render_template('post/create.html')
+    return render_template("post/create.html")
 
 
-@app.route('/post/edit/<int:id>', methods=['GET', 'POST'])
-def edit_one_post(id):
+@app.route("/post/edit/<int:post_id>", methods=["GET", "POST"])
+def edit_one_post(post_id):
     conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?', (id,)).fetchone()
+    post = conn.execute("SELECT * FROM post WHERE id = ?", (post_id,)).fetchone()
     conn.close()
-    
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
+    # Se valida que exista el Post
+    if post is None:
+        return redirect(url_for("get_all_post"))
+
+    if request.method == "POST":
+        title = request.form["title"].strip()
+        content = request.form["content"].strip()
+        # Se valida el título y el contenido
+        if not title or not content:
+            error_msg = "Por favor, ingrese un " + (
+                "título y un contenido."
+                if not title and not content
+                else "título." if not title else "contenido."
+            )
+            return render_template("post/edit.html", post=post, error_msg=error_msg)
 
         conn = get_db_connection()
-        conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, id))
+        conn.execute(
+            "UPDATE post SET title = ?, content = ? WHERE id = ?",
+            (title, content, post_id),
+        )
         conn.commit()
         conn.close()
-        return redirect(url_for('get_all_post'))
+        return redirect(
+            url_for(
+                "get_all_post", success_msg="Se ha actualizado el Post exitosamente."
+            )
+        )
 
-    elif request.method == 'GET':
-        return render_template('post/edit.html', post=post)
+    elif request.method == "GET":
+        return render_template("post/edit.html", post=post)
 
 
-@app.route('/post/delete/<str:post_id>', methods=['POST'])
+@app.route("/post/delete/<int:post_id>", methods=["POST"])
 def delete_one_post(post_id):
     conn = get_db_connection()
-    conn.execute('DELETE FROM posts WHERE id = ?', (post_id))
+    conn.execute("DELETE FROM post WHERE id = ?", (post_id,))
     conn.commit()
     conn.close()
+    return redirect(
+        url_for("get_all_post", success_msg="Se ha eliminado el Post exitosamente.")
+    )
 
-    return redirect(url_for('get_all_post')
 
-if __name__ == '__main__':
-    app.run(debug=True, port=80, host=)
-########################################################################## END BLOQUE 2
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=80)
